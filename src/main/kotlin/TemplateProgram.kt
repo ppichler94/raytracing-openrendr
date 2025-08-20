ca204eca9efb020abad37b427d8adf8d3dddcf90
Patrick Pichler <ppichler@outlook.at>
2025-08-20 21:46:50 +0200

    fix: change scene to cornell box

--- src/main/kotlin/TemplateProgram.kt
+++ src/main/kotlin/TemplateProgram.kt
@@ -1,7 +1,6 @@
 import org.openrndr.application
 import org.openrndr.color.ColorRGBa
 import org.openrndr.draw.colorBuffer
-import org.openrndr.extra.color.presets.PURPLE
 import org.openrndr.math.Vector3
 
 fun main() = application {
@@ -16,12 +15,18 @@ fun main() = application {
         extend(Raytracing()) {
             raysPerPixel(20)
 
-            sphere(Vector3(0.0, 0.0, -1.0), 0.5, ColorRGBa.RED)
-            sphere(Vector3(2.0, 0.5, -0.5), 1.0, ColorRGBa.BLUE)
-            sphere(Vector3(2.0, 2.0, -1.0), 1.5)
-            sphere(Vector3(0.0, -500.5, -1.0), 500.0, ColorRGBa.PURPLE)
+            // Cornell Box centered in the origin (0, 0, 0)
+            val boxHX = 2.6
+            val boxHY = 2.0
+            val boxHZ = 2.8
+            sphere(Vector3(-1e5 - boxHX, .0, .0), 1e5, ColorRGBa.RED) //left
+            sphere(Vector3(1e5 + boxHX, .0, .0), 1e5, ColorRGBa.GREEN) //right
+            sphere(Vector3(.0, -1e5 - boxHY, .0), 1e5) //bottom
+            sphere(Vector3(.0, 1e5 + boxHY, .0), 1e5) //top
+            sphere(Vector3(.0, .0, -1e5 - boxHZ), 1e5) //back
+            sphere(Vector3(.0, .0, 1e5 + boxHZ), 1e5) //front
 
-            light(Vector3(-2.0, 100.0, -100.0), 100.0, ColorRGBa.WHITE, 2.0)
+            light(Vector3(.0, boxHY + 10.0 - .05, .0), 10.0, ColorRGBa.WHITE, 10.0)
 
             output(buffer)
         }
